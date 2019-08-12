8ad9cdff060997781f9669c64e7bf5da4f3e1dac
Felipe Filho <44655339+FelipeDPF@users.noreply.github.com>
2019-08-12 19:41:02 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from "./App";
+
+/**
+ * Create the boilerplate to get React to render something on the screen
+ * Render an <App /> component, which you'll need to create separately
+ */
+ReactDOM.render(<App />, document.getElementById('root'));
